4b6dc087c6812d171fa2d8cb7d06083f57de1273
sonali-borkar123 <borkarsonali7@gmail.com>
2026-03-05 16:12:13 +0530

    ui changes

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,19 +2,7 @@ import Image from "next/image";
 import logoImg from "@/assets/logo.jpeg";
 
 const Logo = ({ size = 40 }: { size?: number }) => (
-  <div
-    className="relative flex-shrink-0 rounded-md overflow-hidden"
-    style={{ height: size, width: size }}
-  >
-    <Image
-      src={logoImg}
-      alt="Noble Engineering logo"
-      fill
-      sizes={`${size}px`}
-      className="object-contain"
-      priority
-    />
-  </div>
+  <Image src={logoImg} alt="Noble Engineering logo" height={size} width={size} className="h-auto" style={{ height: size, width: "auto" }} />
 );
 
 export default Logo;
